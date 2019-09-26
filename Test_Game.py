8b267e8911f223b6772c67242ffb661206313dfd
Sondre Torsdalen <son1709@skole.vtfk.no>
2019-09-26 09:18:50 +0200

    Endringer 09:18 26.09.2019

---
+++ Test_Game.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import pygame
+from pygame.locals import *
+import settings
+import Test_Utility
+# Importerer bare ed classene vi trenger fra sprites filen
+from Test_Sprites import StaticElement, MovingElement, BouncingElement
+
+# Sentrerer pygame vindu
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Initaliserer pygame mondulen
+pygame.init()
+clock = pygame.time.Clock()
+pygame.font.init() # Initaliserer fonter
+
+pygame.key.set_repeat(10, 10)
+
+#screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN | pygame.DOUBLEBUF) 
+screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT)) 
+surface = pygame.Surface(screen.get_size())
+surface.convert()
+
+elements = pygame.sprite.Group()
+
+elements.add(StaticElement(settings.Platform, (100, 100)))
+elements.add(StaticElement(settings.Platform, (200, 200)))
+
+elements.add(MovingElement(settings.Enemy_1, (0, 0), (2,2)))
+elements.add(MovingElement(settings.Enemy_2, (0, 0), (2,1)))
+
+#elements.add(MovingElement(settings.ITEM_ENEMY_BLOCK_RED, (0, 0), (2,2)))
+
+elements.add(BouncingElement(settings.Player_carakter, (0, 300), (2,0)))
+
+elements.add(BouncingElement(settings.Enemy_2, (500, 0), (0,2)))
+
+#elements.add(BouncingElement(settings.Cha, (0, 0), (2,2)))
+
+
+while True:
+    pygame.event.pump()
+    for event in pygame.event.get():
+        # Avslutter ved Window X eller Q tast
+        if (event.type == QUIT) or ((event.type == KEYDOWN) and (event.key == K_ESCAPE)) or ((event.type == KEYDOWN) and (event.key == K_q)):
+            pygame.quit()   
+            sys.exit()
+
+    surface.fill((255, 255, 255))
+    
+    elements.update()
+    elements.draw(surface)
+
+    screen.blit(surface, (0,0))
+ 
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(settings.FPS)
+    # Sjekker om vi traff topp eller bunn av skjermen
---
+++ settings.py
@@ -0,0 +1,22 @@
+DEBUG = True
+
+FPS = 60
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+Gravity = 1
+
+ASSETS_DIR = 'Game_Art'
+#Art-assets
+
+Platform        = {'filename' : 'Platform.png', 'size' : (237,36)}
+Player_carakter = {'filename' : 'playyer.png' , 'size' : (63,150)}
+Enemy_1         = {'filename' : 'oofton.png'  , 'size' : (92,140)}
+Enemy_2         = {'filename' : 'Slime.png'   , 'size' : (78,42)}
+#Bow_1           = {'filename' : 'Bow_1.png'   , 'size' : (x,x)}
+#Bow_2           = {'filename' : 'Bow_2.png'   , 'size' : (x,x)}
+#Arow_1          = {'filename' : 'Arow_1.png'  , 'size' : (x,x)}
+Ground          = {'filename' : 'Test_ground' , 'size' : (569,55)}
+#House           = {'filename' : 'House.png'   , 'size' : (x,x)}
+Backround       = {'filename' : 'Landskap.png', 'size' : (580,326)}
